811d8626c0e2a3d91081e14573c56dc2539f345a
KSP-TaxiService <chinglihlim@gmail.com>
2018-06-16 15:14:14 +0800

    Add KSP setting parameters and upsize delay-bar texture to power of 2

---
+++ src/RemoteTech-Delay/RemoteTechSignalDelayParams.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RemoteTech.Delay
+{
+    public class RemoteTechSignalDelayParams : GameParameters.CustomParameterNode
+    {
+        [GameParameters.CustomStringParameterUI("", autoPersistance = false, lines = 2)]
+        public string description = "To disable signal delay, delete 'RemoteTech-Delay' from RemoteTech's main folder";
+
+        public override string DisplaySection
+        {
+            get
+            {
+                return "RemoteTech";
+            }
+        }
+
+        public override GameParameters.GameMode GameMode
+        {
+            get
+            {
+                return GameParameters.GameMode.ANY;
+            }
+        }
+
+        public override bool HasPresets
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public override string Section
+        {
+            get
+            {
+                return "RemoteTech";
+            }
+        }
+
+        public override int SectionOrder
+        {
+            get
+            {
+                return 2;
+            }
+        }
+
+        public override string Title
+        {
+            get
+            {
+                return "Signal Delay";
+            }
+        }
+    }
+}
